b207bd246c8ebd9f2deef7a3a055a5d340331c38
Heartbleed <hhttiantian@outlook.com>
2019-06-26 13:09:06 +0800

    Dijkstra Algorithm

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -2,7 +2,8 @@
 #include<cstring>
 using namespace std;
 
-const int MAXV = 100;
+const int INF = 1000000000;
+const int MAXV = 1000;
 int matrix[MAXV][MAXV];
 bool visited[MAXV] = { false };
 int dist[MAXV];
@@ -16,9 +17,9 @@ int main()
 {
 	scanf("%d%d%d", &vertex_num,&edge_num, &source);
 	//矩阵的初始化
-	fill(matrix[0], matrix[0] + MAXV * MAXV, INT_MAX);
+	fill(matrix[0], matrix[0] + MAXV * MAXV, INF);
 	int a, b, z;
-	cout << "请输入各边的边的信息（所连的顶点和权数）：" << endl;
+	cout <<"Please enter info:"<< endl;
 	for(int i=0;i<edge_num;i++)
 	{
 		scanf("%d%d%d", &a,&b,&z);
@@ -31,7 +32,7 @@ int main()
 	{
 		if (i != source)
 		{
-			printf("%d到%d的最短距离是：%d，路径是：", source, i, dist[i]);
+			printf("%d to %d shortest path length is: %d, the path is: ", source, i, dist[i]);
 			printPath(i);
 			printf("\n");
 		}
@@ -42,53 +43,46 @@ int main()
 
 void Dijkstra(int source) 
 {
-	visited[source] = true;
+	fill(dist, dist + MAXV, INF);
+  	dist[source] = 0;	
 
-	for (int i = 0; i < vertex_num; i++)
-	{
-		dist[i] = matrix[source][i];
-		path[i] = source;
-	}
-
-	int min_cost; // 最小权值
-	int min_cost_index; // 最小权值的下标
+	for(int i = 0; i < vertex_num; i++) 
+		path[i] = i;
 
 	for (int i = 1; i < vertex_num; i++)
 	{
-		min_cost = INT_MAX;
+		int u = -1, min = INF; 
 		for (int j = 0; j < vertex_num; j++)
 		{
-			if (visited[j] == false && dist[j] < min_cost)
+			if (visited[j] == false && dist[j] < min)
 			{
-				min_cost = dist[j];
-				min_cost_index = j;
+				u = j;
+				min = dist[j];
 			}
 		}
 
-		visited[min_cost_index] = true;
+		if(min == -1) return;
+		visited[u] = true;
 
 		for (int j = 0; j < vertex_num; j++)
 		{
 			if (visited[j] == false &&
-				matrix[min_cost_index][j] != INT_MAX &&
-				matrix[min_cost_index][j] + min_cost < dist[j])
+				matrix[u][j] != INF &&
+				matrix[u][j] + dist[u] < dist[j])
 			{
-				dist[j] = matrix[min_cost_index][j] + min_cost;
-				path[j] = min_cost_index;
+				dist[j] = matrix[u][j] + dist[u];
+				path[j] = u;
 			}
 		}
 	}
 }
 
-void printPath(int i)
-{
-    int tmp = source;
-    if(i == source)
-    {
-        printf("%d ", source);
-        return;
+void printPath(int v) {
+    if(v == source) {
+        printf("%d ", v);
+        return ;
     }
-    printPath(path[i]);
-    printf("%d ", i);
+    printPath(path[v]);
+    printf("%d ", v);
 }
 
